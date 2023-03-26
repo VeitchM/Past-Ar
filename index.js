b81cc56437df8251fa27daf4c7cf1ee009261a8a
Matias <matiasveitch@gmail.com>
2023-03-25 21:57:09 -0300

    Restructured Folders

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+import App from './src/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+console.log('Index.js is executed');
 registerRootComponent(App);
