86ac067bcd90466c97fccb0ac42a1157cb428344
RenzoRomeo <renzoromeo44@gmail.com>
2024-08-24 13:16:56 -0300

    changed api endpoint and config

--- src/features/backend/config.ts
+++ src/features/backend/config.ts
@@ -1,4 +1,3 @@
-import { backend } from '../../../config.json'
-const backendURL = backend.URL
-const backendAPIVersion = backend.API_VERSION
-export const mobileAPI = `${backendURL}/mobile-api/${backendAPIVersion}`
+import { backend } from "../../../config.json";
+const backendURL = backend.URL;
+export const mobileAPI = `${backendURL}/mobile-api`;
