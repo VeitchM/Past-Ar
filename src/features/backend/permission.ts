534101272dc3645c1841be4e6d2bcb47ea4e605e
RenzoRomeo <renzoromeo44@gmail.com>
2024-08-28 20:13:37 -0300

    temporarily disabled permission checks

--- src/features/backend/permission.ts
+++ src/features/backend/permission.ts
@@ -1,39 +1,50 @@
-import store from "../store/store"
+import store from "../store/store";
 
 export enum Roles {
-    WORKER = 'worker',
-    VIEWER = 'viewer',
-    ADMIN = 'admin',
-    OWNER = 'owner'
+  WORKER = "worker",
+  VIEWER = "viewer",
+  ADMIN = "admin",
+  OWNER = "owner",
 }
 
 export default class Permission {
-    private static hasPermission(permissions: String[]): boolean {
-        const user = store.getState().backend.user;
-        const roles = (user && user.roles) ? user.roles : [];
-        return !!roles ? roles.map(rol => permissions.includes(rol)).includes(true) : false
-    }
-    static getMeasurements(): boolean {
-        return Permission.hasPermission([Roles.OWNER, Roles.ADMIN, Roles.VIEWER])
-    }
+  private static hasPermission(permissions: String[]): boolean {
+    return true;
+    // const user = store.getState().backend.user;
+    // const roles = (user && user.roles) ? user.roles : [];
+    // return !!roles ? roles.map(rol => permissions.includes(rol)).includes(true) : false
+  }
+  static getMeasurements(): boolean {
+    return Permission.hasPermission([Roles.OWNER, Roles.ADMIN, Roles.VIEWER]);
+  }
 
-    static postMeasurements(): boolean {
-        return Permission.hasPermission([Roles.OWNER, Roles.ADMIN, Roles.WORKER])
-    }
+  static postMeasurements(): boolean {
+    return Permission.hasPermission([Roles.OWNER, Roles.ADMIN, Roles.WORKER]);
+  }
 
-    static getCalibrations(): boolean {
-        return Permission.hasPermission([Roles.OWNER, Roles.ADMIN, Roles.WORKER,Roles.VIEWER])
-    }
+  static getCalibrations(): boolean {
+    return Permission.hasPermission([
+      Roles.OWNER,
+      Roles.ADMIN,
+      Roles.WORKER,
+      Roles.VIEWER,
+    ]);
+  }
 
-    static postCalibrations(): boolean {
-        return Permission.hasPermission([Roles.OWNER, Roles.ADMIN, Roles.WORKER])
-    }
+  static postCalibrations(): boolean {
+    return Permission.hasPermission([Roles.OWNER, Roles.ADMIN, Roles.WORKER]);
+  }
 
-    static getPaddocks(): boolean {
-        return Permission.hasPermission([Roles.OWNER,Roles.ADMIN,Roles.WORKER,Roles.VIEWER])
-    }
+  static getPaddocks(): boolean {
+    return Permission.hasPermission([
+      Roles.OWNER,
+      Roles.ADMIN,
+      Roles.WORKER,
+      Roles.VIEWER,
+    ]);
+  }
 
-    static postPaddocks(): boolean {
-        return Permission.hasPermission([Roles.OWNER, Roles.ADMIN, Roles.WORKER])
-    }
+  static postPaddocks(): boolean {
+    return Permission.hasPermission([Roles.OWNER, Roles.ADMIN, Roles.WORKER]);
+  }
 }
