4203e3277c61e385f6ccda7a19f5d67434a04222
RenzoRomeo <renzoromeo44@gmail.com>
2024-09-18 21:07:17 -0300

    fix: cálculo peso medio

--- src/features/statistics/WeightCalculus.ts
+++ src/features/statistics/WeightCalculus.ts
@@ -1,28 +1,16 @@
 import { getCalibrations } from "../localDB/calibrations";
-import { Measurement } from "../store/types";
-import React from "react";
 
-
-export default class WeightCalculus {
-
-}
-
-export async function calculateByHeight(height:number,calibrationID:number) {
-    let cal = await getCalibrations();
-    let filtered = cal.filter(e=>{return e.ID == calibrationID});
-    if (filtered.length > 0 && filtered[0].function){
-        return (processFunction(height, filtered[0].function!.split(',').map(_ => { return parseInt(_) })))
-    }
-    return 0;
+export async function calculateByHeight(height: number, calibrationID: number) {
+  const cal = await getCalibrations();
+  const [calibration] = cal.filter((e) => {
+    return e.ID == calibrationID;
+  });
+  if (calibration && calibration.function) {
+    return processFunction(height, calibration.function.split(",").map(Number));
+  }
+  return 0;
 }
 
 function processFunction(h: number, fun: number[]) {
-    let res = 0;
-    fun.forEach((v,i)=>{
-        if (i==0) res+=v;
-        else{
-            res+=v*Math.pow(h,i);
-        }
-    })
-    return res;
+  return Math.floor(fun[0] * h + fun[1]);
 }
